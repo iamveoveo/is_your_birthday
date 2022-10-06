12de7f60098b846a8c034f65265cf727bb24dd07
iamveoveo <vinhveoveo21@gmail.com>
2022-10-06 22:22:43 +0700

    video changed

--- js/hello_lan.js
+++ js/hello_lan.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         if ($(this).attr('name') == 'piggy') {
             $('#wish').text('Chúc mừng sinh nhật bạn yêu nhá. Trên này không viết được quá nhiều nên hãy direct inbox với t để nghe những lời âu yếm nhá hí hí');
         } else if ($(this).attr('name') == 'flower') {
-            window.open('https://drive.google.com/file/d/19ehlvg2m8M685ZAy5bVXRsh4TYuW8p_a/view?usp=sharing', '_blank');
+            window.open('https://photos.app.goo.gl/soM7RUKV1g8WgnSP8', '_blank');
         } else if ($(this).attr('name') == 'poke') {
             $('#wish').text('Yêu Nóc');
         } else {
