a39fc81f960d04e495b75fc6d9a6e02056924209
iamveoveo <vinhveoveo21@gmail.com>
2022-10-06 20:49:26 +0700

    video added for flower wish

--- js/hello_lan.js
+++ js/hello_lan.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         if ($(this).attr('name') == 'piggy') {
             $('#wish').text('Chúc mừng sinh nhật bạn yêu nhá. Trên này không viết được quá nhiều nên hãy direct inbox với t để nghe những lời âu yếm nhá hí hí');
         } else if ($(this).attr('name') == 'flower') {
-            window.open('https://www.canva.com/design/DAFODiM8rtg/37fTgS34ZcDPQPlsrOGBHA/watch?utm_content=DAFODiM8rtg&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink', '_blank');
+            window.open('https://drive.google.com/file/d/1d26Y1WqBqLt73fAqT_-vFKjINw_DLrQz/view?usp=sharing', '_blank');
         } else if ($(this).attr('name') == 'poke') {
             $('#wish').text('Yêu Nóc');
         } else {
