522c866a8b88e05299ededa186692d6277434540
iamveoveo <76905625+iamveoveo@users.noreply.github.com>
2025-10-27 22:44:44 +0700

    Update hello_lan.js

--- js/hello_lan.js
+++ js/hello_lan.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('.character').on('click', function(){
         if ($(this).attr('name') == 'piggy') {
-            $('#wish').text('Chúc mừng sinh nhật bạn yêu nhá. Trên này không viết được quá nhiều nên hãy direct inbox với t để nghe những lời âu yếm nhá hí hí');
+            $('#wish').text('Chúc mừng sinh nhật em gái yêu nhá. Tuổi mới ít làm và tăng hưởng thụ nha em gái. Chúc em né qua được tất cả các tiêu cực nhé dù nó có ở đâu, có là gì đi nữa.');
         } else if ($(this).attr('name') == 'flower') {
-            window.open('https://photos.app.goo.gl/soM7RUKV1g8WgnSP8', '_blank');
+            window.open('https://www.youtube.com/watch?v=5u4xTa3LR2U&list=RD5u4xTa3LR2U&start_radio=1', '_blank');
         } else if ($(this).attr('name') == 'poke') {
-            $('#wish').text('Yêu Nóc');
+            $('#wish').text('Giữ sự năng động nha cô gái.');
         } else {
             alert('Chúc mừng sinh nhật');
         }
